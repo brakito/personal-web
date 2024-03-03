c59a502d81d8af2280d52fb1dee16a2d7dec37b0
Brakito <arangobrmst@gmail.com>
2024-03-03 15:56:17 -0500

    Add Portfolio page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+			dropShadow: {
+				glow: [
+					"0 0px 20px rgba(127,18,255,.4)",
+					"0 0px 45px rgba(127,18,255,.3)",
+				]
+			},
 			
 		},
 	},

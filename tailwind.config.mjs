bb8e4027c18bd0ffc98280c68c4061ceed26c512
Brakito <arangobrmst@gmail.com>
2024-03-03 16:21:43 -0500

    Fix font config to use Open Sans

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,7 @@ export default {
 	theme: {
 		extend: {
 			fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        sans: ['"Open Sans"', ...defaultTheme.fontFamily.sans],
       },
 			dropShadow: {
 				glow: [
